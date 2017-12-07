c3be905b09cc0ce187200c21d9ab8fe7032733f6
Ashish <aashishvyass@gmail.com>
2017-12-07 15:28:47 +0530

    comit

---
+++ run.bat
@@ -0,0 +1,5 @@
+set projectLocation=H:\MobileAutomation\Jenkins
+cd %projectLocation%
+set classpath=%projectLocation%\bin\;%projectLocation%\lib\*
+java org.testng.TestNG %projectLocation%\testng.xml
+pause
